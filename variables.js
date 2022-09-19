fcea944e4aa4784412c1e9c542160b78bb9b87e2
ParkJW <pc224538@gmail.com>
2022-09-19 12:21:34 +0900

    commit
    first commit

---
+++ variables.js
@@ -0,0 +1,18 @@
+var a = 5
+console.log("a의 자료형 : " + typeof a)
+var b = 4.0 // number 
+console.log("b의 자료형 : " + typeof b)
+var c = "test" // string
+console.log(`c의 자료형 : ${typeof c}`)
+var d = 't' // char
+var e = false // boolean
+var f = null
+var g // undefined
+var h = [1, 2, 3] // array
+var i = {
+    name: 'seon', 
+    job: 'tutor'
+}
+var j = function () {
+    console.log("hello javascript world");
+}
